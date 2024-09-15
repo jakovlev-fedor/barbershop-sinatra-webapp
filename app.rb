d60c237a604709da4a467454a609975248132d37
Fedor Yakovlev <jakovlev.fedor.me@gmail.com>
2024-09-15 22:20:33 +0300

    Add basic functionality

--- app.rb
+++ app.rb
@@ -1,13 +1,35 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'json'
 
 configure do
   enable :sessions
 end
 
 get '/' do
-  erb :invite
+  erb :home
+end
+
+get '/appointment/form' do
+  erb :appointment_form
+end
+
+post '/appointment/submit' do
+  @title = 'Success!'
+  @customer_name = params[:customer_name]
+  @customer_phone = params[:customer_phone]
+  @appointment_date = params[:appointment_date]
+  @appointment_time = params[:appointment_time]
+
+  data = { name: @customer_name,
+           phone: @customer_phone,
+           date: @appointment_date,
+           time: @appointment_time }
+
+  save_appointment(data)
+
+  erb :appointment_submit
 end
 
 get '/login/form' do
@@ -19,11 +41,11 @@ get '/logout' do
   erb :logout
 end
 
-get '/secret/place' do
-  erb :secret_place
+get '/admin' do
+  erb :admin
 end
 
-before '/secret/*' do
+before '/admin' do
   unless session[:identity]
     session[:previous_url] = request.path
     @error = "Sorry, in order to visit '#{request.path}' you must log in"
@@ -38,7 +60,7 @@ post '/login/attempt' do
   password = params['password']
   if username == 'admin' && password == 'admin'
     session[:identity] = params['username']
-    where_user_came_from = session[:previous_url] || '/'
+    where_user_came_from = session[:previous_url] || '/admin'
     redirect to where_user_came_from
   else
     @error = 'Wrong username or password'
@@ -51,5 +73,17 @@ end
 helpers do
   def username
     session[:identity] ? session[:identity] : 'Hello stranger'
+  end
+
+  def time_stamp
+    t = Time.now
+    t.strftime('%Y%m%d-%H%M%S')
+  end
+
+  def save_appointment(data)
+    f = File.open('customers.jsonl', 'a')
+    buffer = { time_stamp => data }
+    f.write("#{buffer.to_json}\n")
+    f.close
   end
 end
